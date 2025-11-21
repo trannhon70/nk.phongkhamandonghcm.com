51a6640f5863ba2490159c64728330228eb5878c
trannhon70 <kevintran351996@gmail.com>
2025-11-21 09:42:52 +0700

    update

--- index.php
+++ index.php
@@ -41,6 +41,24 @@ $local = 'https://nk.phongkhamandonghcm.com';
             document.removeEventListener('scroll', loadGA);
         });
     </script>
+    <!-- Google Tag Manager -->
+    <script defer>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PX2JGH6D');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 
