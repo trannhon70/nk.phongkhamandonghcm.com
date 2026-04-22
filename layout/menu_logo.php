44b5162396f7a2d7534fda94c3f212c064a31f9b
trannhon70 <kevintran351996@gmail.com>
2026-04-22 16:58:55 +0700

    update

--- layout/menu_logo.php
+++ layout/menu_logo.php
@@ -1,24 +1,23 @@
-<div class="dakhoa__motto-fox" >
+<div class="dakhoa__motto-fox">
     <div class="dakhoa__motto-container-row">
-        <a href="<?php echo $local ?>">
+        <a href="javascript:void(0)" onclick="openZoosUrl('chatwin'); return false;">
             <div class="dakhoa__motto-container-row-card">
                 <amp-img class="dakhoa__motto-container-row-card-img" src="images/icons/icon_noikhoa.webp" height="120px" width="120px" alt="..."></amp-img>
                 <div>Tư vấn</div>
             </div>
         </a>
-        <a href="<?php echo $local ?>">
+        <a href="javascript:void(0)" onclick="openZoosUrl('chatwin'); return false;">
             <div class="dakhoa__motto-container-row-card">
                 <amp-img class="dakhoa__motto-container-row-card-img" src="images/icons/icon_ngoaikhoa.webp" height="120px" width="120px" alt="..."></amp-img>
                 <div>Đặt lịch</div>
             </div>
-        </a
-        ><a href="<?php echo $local ?>">
+        </a><a href="javascript:void(0)" onclick="openZoosUrl('chatwin'); return false;">
             <div class="dakhoa__motto-container-row-card">
                 <amp-img class="dakhoa__motto-container-row-card-img" src="images/icons/icon_dalieu.webp" height="120px" width="120px" alt="..."></amp-img>
                 <div>Thăm khám</div>
             </div>
         </a>
-        <a href="<?php echo $local ?>">
+        <a href="javascript:void(0)" onclick="openZoosUrl('chatwin'); return false;">
             <div class="dakhoa__motto-container-row-card">
                 <amp-img class="dakhoa__motto-container-row-card-img" src="images/icons/icon_hmtt.webp" height="120px" width="120px" alt="..."></amp-img>
                 <div>Hỗ trợ điều trị</div>
